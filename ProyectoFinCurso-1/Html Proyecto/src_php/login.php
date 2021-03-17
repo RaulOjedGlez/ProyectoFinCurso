969bb5ea9f66d7f1713972d305610554e4c5946c
Ruli <57179176+RaulOjedGlez@users.noreply.github.com>
2021-03-17 13:15:00 +0000

    Delete ProyectoFinCurso-1/Html Proyecto directory

--- ProyectoFinCurso-1/Html Proyecto/src_php/login.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-if(isset($_POST["usuario"], $_POST["contra"])){
-    $_SESSION["usuario"] = $_POST["usuario"];
-
-    echo "Hola " . $_SESSION["usuario"] . " a que alumno quieres buscar?";
-}
-
-?>
